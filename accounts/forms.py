5e2bf22f34cf622fbf075fa1b3f3d3d995874146
awaisyasin <awaisyasin.co@gmail.com>
2024-03-01 16:03:28 +0500

    Add head text in guest and host sign up form

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,11 +7,6 @@ from . import models
 # Create your forms here.
 
 class GuestSignUpForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.required = True
-
     class Meta:
         model = models.GuestProfile
         fields = ['first_name', 'last_name', 'year_of_birth', 'email', 'password1', 'password2']
--- accounts/models.py
+++ accounts/models.py
@@ -5,12 +5,17 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 class GuestProfile(AbstractUser):
+    first_name = models.CharField()
+    last_name = models.CharField()
     profile_image = models.ImageField(null=True, blank=True)
     YEAR_CHOICES = [
         (year, str(year)) for year in range(1920, 2024)
     ]
     year_of_birth = models.PositiveIntegerField(choices=YEAR_CHOICES)
-    email_verification_token = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField()
+    password1 = models.CharField()
+    password2 = models.CharField()
+    email_verification_token = models.CharField(max_length=255, null=True, blank=True)
     is_email_verified = models.BooleanField(default=False)
 
     groups = models.ManyToManyField(
@@ -21,7 +26,7 @@ class GuestProfile(AbstractUser):
             "The groups this user belongs to. A user will get all permissions "
             "granted to each of their groups."
         ),
-        related_name="guest_set",
+        related_name="guest_profiles",
         related_query_name="guest",
     )
 
@@ -30,18 +35,23 @@ class GuestProfile(AbstractUser):
         verbose_name=_("user permissions"),
         blank=True,
         help_text=_("Specific permissions for this user."),
-        related_name="guest_set",
+        related_name="guest_profiles",
         related_query_name="guest",
     )
 
 
 class HostProfile(AbstractUser):
+    first_name = models.CharField()
+    last_name = models.CharField()
     profile_image = models.ImageField(null=True, blank=True)
     YEAR_CHOICES = [
         (year, str(year)) for year in range(1920, 2024)
     ]
     year_of_birth = models.PositiveIntegerField(choices=YEAR_CHOICES)
+    email = models.EmailField()
     podcast_name = models.CharField(max_length=255)
+    password1 = models.CharField()
+    password2 = models.CharField()
     email_verification_token = models.CharField(max_length=200, null=True, blank=True)
     is_email_verified = models.BooleanField(default=False)
 
@@ -62,6 +72,6 @@ class HostProfile(AbstractUser):
         verbose_name=_("user permissions"),
         blank=True,
         help_text=_("Specific permissions for this user."),
-        related_name="guest_profiles",
+        related_name="host_profiles",
         related_query_name="host",
     )
